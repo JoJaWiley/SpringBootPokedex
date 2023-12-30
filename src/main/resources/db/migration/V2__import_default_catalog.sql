54300e503e47a0b73cd26a673dc1e41f34a5e2ec
JoJaWiley <joshua.wiley@summitoic.edu>
2023-12-30 17:08:45 -0600

    db migrations bugfix - delete scripts and recreate them to update them.

---
+++ src/main/resources/db/migration/V2__import_default_catalog.sql
@@ -0,0 +1,4 @@
+INSERT INTO pokemon (name, type) VALUES ('Bulbasaur', 'Grass');
+INSERT INTO pokemon (name, type) VALUES ('Ivysaur', 'Grass');
+INSERT INTO pokemon (name, type) VALUES ('Venusaur', 'Grass');
+INSERT INTO pokemon (name, type) VALUES ('Charmander', 'Fire');
