26fdb7a2e9b2722c940751d9bd3a8544e39afd3b
JoJaWiley <joshua.wiley@summitoic.edu>
2023-12-30 16:51:20 -0600

    Added JpaCatalogService, PokemonRepository, db migrations (still seem buggy), and properly annotated the CatalogController. Both integration tests for CatalogController pass.

--- src/main/java/com/example/pokemondb/catalog/CatalogController.java
+++ src/main/java/com/example/pokemondb/catalog/CatalogController.java
@@ -1,11 +1,22 @@
 package com.example.pokemondb.catalog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
+@Controller
 public class CatalogController {
 
+    private final CatalogService catalogService;
+
+    CatalogController(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
     @GetMapping("/")
-    String index() {
-        return "index";
+    ModelAndView index() {
+        return new ModelAndView("index", Map.of("pokemon", this.catalogService.getAllPokemon()));
     }
 }
